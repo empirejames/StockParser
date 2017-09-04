56930953950f0b11a18fa67c25260ce56a68ad09
James.chu <James.chu@everspring.com>
2017-09-04 10:21:02 +0800

    Comit for V3.6 version for hidding other login and notification bug

--- app/src/main/java/com/james/stockparser/MyFirebaseMessagingService.java
+++ app/src/main/java/com/james/stockparser/MyFirebaseMessagingService.java
@@ -22,7 +22,7 @@ public class MyFirebaseMessagingService  extends FirebaseMessagingService {
     }
     private void sendNotification(RemoteMessage remoteMessage) {
 
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, LoginActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 , intent, PendingIntent.FLAG_ONE_SHOT);
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
